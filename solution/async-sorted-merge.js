4b336b4f65745c7b51614fcc70bac0142feb7c94
Rochelle Valdez <rochellevee@gmail.com>
2017-09-08 16:12:57 -0700

    Pseudocode solution to async-sorted-merge

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,5 +1,17 @@
 'use strict'
 
 module.exports = (logSources, printer) => {
-	throw new Error('Not implemented yet!  That part is up to you!')
+	//create storage variable for promises named 'logs'
+
+  //iterate over all logSources
+
+    //store all promises into logs array
+
+  //Return all promises with passed logs array
+
+    //sort resolved array
+
+    //print each log from sorted array
+
+    //call printer.done() for stats
 }
