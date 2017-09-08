6afe58097c7a6586f41ec01677df2819c6f9cb40
Rochelle Valdez <rochellevee@gmail.com>
2017-09-08 16:07:45 -0700

    Pseudocoded solution to sync-sorted-merge

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,5 +1,17 @@
 'use strict'
 
 module.exports = (logSources, printer) => {
-	throw new Error('Not implemented yet!  That part is up to you!')
+  //declare storage array 'logs'
+
+  //iterate over logSources
+  
+  //for each logSource
+  
+    //push into logs array
+
+  //fast-sort array, call sortedArray
+
+  //print each log of sortedArray
+
+  //call done for stats
 }
